b6ad6a571e5f4534a6e302bf1bcb17c06a6099d6
Onelio Viera <ojvwebdesign@gmail.com>
2023-10-02 08:24:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# MERN Project-v2
+
+## Backend Development
+
+### 1. Create a new project folder
+
+```bash 
+mkdir mern-project-v2```
---
+++ backend/server.js
@@ -0,0 +1,3 @@
+const express = require('express') // Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.
+
+const dotenv = require('dotenv') // dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
